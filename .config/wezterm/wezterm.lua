4744e8e1a367b549705d50f5e0c6da1e04cd0a7f
Reed Merrill <reed@Reeds-MacBook-Pro.local>
2024-09-04 08:45:05 -0600

    maybe fixed weird colours

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,8 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.set_environment_variables = { COLORTERM = "truecolor" }
+
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:

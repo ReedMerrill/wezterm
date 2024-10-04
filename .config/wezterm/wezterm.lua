e0202177065cba1ce2e90b1218f4bae01c0a9d67
Reed Merrill <reed@Reeds-MacBook-Pro.local>
2024-10-04 16:32:41 -0600

    disabled some keyboards

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,6 +13,25 @@ config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
 config.font = wezterm.font("0xProto Nerd Font Mono", { weight = "Regular" })
 config.font_size = 13.5
+-- disable keys that conflict with neovim and tmux
+config.keys = {
+	-- disable <C-T>, which create a new wezterm tab
+	{
+		key = "T",
+		mods = "CTRL",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "N",
+		mods = "CTRL",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "P",
+		mods = "CTRL",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
